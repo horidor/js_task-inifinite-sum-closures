4e476f911ae3661451b97f9302103638e8d5bae1
Maxim <makson202007@gmail.com>
2025-07-21 12:01:32 +0300

    added task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let result = 0;
+
+  const adder = (a) => {
+    if (a === undefined) {
+      const finalResult = result;
+
+      result = 0;
+
+      return finalResult;
+    }
+
+    result += a;
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
