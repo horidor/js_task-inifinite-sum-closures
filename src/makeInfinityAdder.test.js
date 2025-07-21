108810b21e871220a4646a77474b8d0512a60a95
Maxim <makson202007@gmail.com>
2025-07-21 23:26:26 +0300

    ??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????

--- src/makeInfinityAdder.test.js
+++ src/makeInfinityAdder.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const makeAdder = require('./makeInfinityAdder');
+const makeAdder = require('./makeAdder');
 
 test('The result of makeInfinityNumber should be a function', () => {
   const adder = makeAdder();
